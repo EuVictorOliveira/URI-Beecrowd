92c54ca50d9d840cd74321c4577dec3554e4bf98
Victor Oliveira <victoroliver249@gmail.com>
2025-04-05 18:39:50 -0300

    Ajusta beecrowd1002 em java

--- Iniciante/Java/Beecrowd1002.java
+++ Iniciante/Java/Beecrowd1002.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Beecrowd{
+public class Beecrowd1002{
     public static void main(String[] args){
         Scanner leitor = new Scanner(System.in);
         double area, raio, pi = 3.14159;
